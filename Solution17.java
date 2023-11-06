f37431a953344c54ff08662fe3ba1abfdd0b6b37
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2023-11-07 00:04:33 +0900

    진료 순서 정하기
    진료 순서 정하기

---
+++ Solution17.java
@@ -0,0 +1,18 @@
+package beginner;
+
+//진료 순서 정하기
+class Solution17 {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        
+        for(int i = 0 ; i < emergency.length; i++) {
+            for(int j = 0 ; j < emergency.length; j++) {
+                if(emergency[i] < emergency[j]) {
+                    answer[i]++;
+                }
+            }
+            answer[i]++;
+        }
+        return answer;
+    }
+}
