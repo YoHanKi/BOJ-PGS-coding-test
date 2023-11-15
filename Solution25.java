5f41c9ba3aa68fe195e37de9215d3ed459c3924e
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2023-11-15 20:24:44 +0900

    최댓값 만들기
    최댓값 만들기

---
+++ Solution25.java
@@ -0,0 +1,13 @@
+package beginner;
+
+//최댓값 만들기
+import java.util.Arrays;
+
+class Solution25 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        return answer 
+            = numbers[numbers.length-1] * numbers[numbers.length-2];
+    }
+}
