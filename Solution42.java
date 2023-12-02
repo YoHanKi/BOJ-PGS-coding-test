1e4581598f52ea83fbf6de1a48c44c9ec75f9c45
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2023-12-02 18:54:28 +0900

    문자열 계산하기
    문자열 계산하기

---
+++ Solution42.java
@@ -0,0 +1,20 @@
+package beginner;
+
+//문자열 계산하기
+class Solution42 {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        String[] s = my_string.split(" ");
+        answer = Integer.parseInt(s[0]);
+        
+        for (int i=1; i<s.length-1; i++) {
+            if (s[i].equals("+")) {
+                answer += Integer.parseInt(s[i+1]);
+            } else if (s[i].equals("-")) {
+                answer -= Integer.parseInt(s[i+1]);
+            }
+        }
+        return answer;
+    }
+}
