9d356d65f496ca2690e7f5aa6abc2a0886fa582c
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2023-12-18 15:31:02 +0900

    최댓값 만들기
    최댓값 만들기

---
+++ Solution58.java
@@ -0,0 +1,16 @@
+package beginner;
+
+//최댓값 만들기
+class Solution58 {
+	    public int solution(int[] numbers) {
+	        int answer = Integer.MAX_VALUE * -1;
+	        
+	        for(int i=0; i < numbers.length ; i++){
+	            for(int j = 0; j < numbers.length ; j++){
+	                if(answer < numbers[i] * numbers[j] && i != j) 
+	                    answer = numbers[i] * numbers[j];
+	            }
+	        }
+	        return answer;
+	    }
+	}
