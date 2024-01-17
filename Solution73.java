1cd2997774e36bc515d0cdb11e192df0c254eb31
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-01-17 17:28:03 +0900

    문자열 밀기
    문자열 밀기

---
+++ Solution73.java
@@ -0,0 +1,21 @@
+package beginner;
+
+//문자열 밀기
+class Solution73 {
+    public int solution(String A, String B) {
+        int answer = 0;
+        String copy = A;
+
+        for (int i = 0; i < A.length(); i++) {
+            if (copy.equals(B)) {
+                return answer;
+            }
+
+            String a = copy.substring(copy.length() - 1);
+            copy = a + copy.substring(0, copy.length() - 1);
+            answer++;
+        }
+
+        return -1;
+    }
+}
