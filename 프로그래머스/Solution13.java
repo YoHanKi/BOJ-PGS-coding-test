d5c6f768ac02c5126d9ab4dc18b044599b393ea5
기묘한 주니어 개발자 <alcuz137@gmail.com>
2024-01-31 23:32:51 +0900

    update

--- 프로그래머스/Solution13.java
+++
@@ -1,9 +0,0 @@
-package beginner;
-
-//특정문자제거
-class Solution13 {
-    public String solution(String my_string, String letter) {
-        String answer = my_string.replace(letter,"");
-        return answer;
-    }
-}
