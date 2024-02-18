426bf388319dda8cf353064d0a1a8e5141075700
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-02-19 01:57:24 +0900

    [level 3] Title: 베스트앨범, Time: 1.13 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 1.23 ms
+메모리: 72.9 MB, 시간: 1.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 1일 1:48:36
+2024년 2월 1일 1:57:20
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+//인덱스와 플레이 수를 저장해줄 클래스 생성
 class Music {
     public int index;
     public int play;
@@ -11,21 +12,28 @@ class Music {
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
+        //가장 큰 장르를 찾을 totalMap
         Map<String, Integer> totalMap = new HashMap<>();
+        //Music 클래스를 저장하고, musicSort에 저장할 리스트와 맵
         List<Music> list = new ArrayList<>();
         Map<String, List<Music>> musicSort = new HashMap<>();
 
         for (int i = 0; i < genres.length; i++) {
+            //getOrDefault로 가장 높은 플레이 장르 찾기
             totalMap.put(genres[i], totalMap.getOrDefault(genres[i], 0) + plays[i]);
+            //장르가 null 이라면 new ArrayList 생성
             if (musicSort.get(genres[i]) == null) {
                 musicSort.put(genres[i], new ArrayList<>());
             }
+            //리스트에 Music 추가
             musicSort.get(genres[i]).add(new Music(i,plays[i]));
         }
-        
+        //totalMap을 내림차순으로 정렬하기 위해 entryset 추가
         List<Map.Entry<String, Integer>> entries = new ArrayList<>(totalMap.entrySet());
+        //Collections.sort의 두번째 파라미터는 람다식으로 정렬기준을 지정할 수 있음(Comarator 인스턴스 생성, compare 오버라이딩하는 방법이 있으나 너무 복잡함)
         Collections.sort(entries, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-
+        
+        //musicSort 내부의 리스트를 돌면서 정렬
         for (String genre : musicSort.keySet()) {
             List<Music> musics = musicSort.get(genre);
             musics.sort((m1, m2) -> {
@@ -33,11 +41,13 @@ class Solution {
                 else return m2.play - m1.play;
             });
         }
-        
+        //장르가 1개인 경우도 있으므로 유동적인 ArrayList 사용
         List<Integer> result = new ArrayList<>();
+        //정렬된 entries 를 사용하여 index를 result에 저장
         for (Map.Entry<String, Integer> entry : entries) {
             List<Music> musics = musicSort.get(entry.getKey());
-            int songsToAdd = Math.min(musics.size(), 2); // 해당 장르의 곡 수가 2 미만인 경우를 처리
+            // 해당 장르의 곡 수가 2 미만인 경우 musics.size로 반복문 지정
+            int songsToAdd = Math.min(musics.size(), 2); 
             for (int i = 0; i < songsToAdd; i++) {
                 result.add(musics.get(i).index);
             }
