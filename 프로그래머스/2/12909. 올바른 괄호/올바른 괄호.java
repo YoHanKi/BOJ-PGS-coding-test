4c86bd8fa0b4b46698c3fb53820e4791f96034b5
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-02-19 09:32:06 +0900

    Update 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
-class Solution {
-    boolean solution(String s) {
-        boolean answer = false;
-        char[] strArr = s.toCharArray();
+public class WeeklyQuizFourth {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Character> stack = new Stack<>();
+        
+        String s = br.readLine();
+        char[] strArr = s.toCharArray();
 
         for(char i : strArr) {
             if(i == '(') {
                 stack.push(i);
             } else if(stack.isEmpty()) {
-                return answer;
+                System.out.println(false);
+             return;
             } else stack.pop();
-            
         }
-        
-        if (!(stack.size() == 0)) return answer;
-        return answer = true;
+        if (!(stack.size() == 0)) System.out.println(false);
+        else System.out.println(true);
     }
 }
