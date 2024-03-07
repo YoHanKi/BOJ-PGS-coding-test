d9f4651937c0e9e9ade725db4023c7398146d49f
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-03-07 18:27:04 +0900

    [level 2] Title: H-Index, Time: 0.37 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,19 +1,29 @@
 import java.util.*;
  
-public class Solution {
+// public class Solution {
+//     public int solution(int[] citations) {
+//         int answer = 0;
+//         Arrays.sort(citations);
+ 
+//         for (int i = 0; i < citations.length; i++) {
+//             int h = citations.length - i;
+ 
+//             if (citations[i] >= h) {
+//                 answer = h;
+//                 break;
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        Arrays.sort(citations);
- 
-        for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i;
- 
-            if (citations[i] >= h) {
-                answer = h;
-                break;
-            }
+      
+        Arrays.sort(citations);    
+        for(int i=citations.length; i>0; i--){
+            if(citations[citations.length-i] >= i) return i;
         }
- 
-        return answer;
+        return 0;
     }
 }
