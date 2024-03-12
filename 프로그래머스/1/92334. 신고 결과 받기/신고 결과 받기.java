c0725301465a02f3aa3358b0a5c63ca60ade4824
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-03-12 23:52:21 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.08 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,26 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
+        int[][] reports = new int[id_list.length][id_list.length];
         Map<String, Integer> user = new HashMap<>();
         int[] counts = new int[id_list.length];
-        int[][] reports = new int[id_list.length][id_list.length];
         int[] answer = new int[id_list.length];
         
-        for(int i = 0; i < id_list.length; i++) user.put(id_list[i], i);
+        for(int i = 0; i < id_list.length; i++) 
+        user.put(id_list[i], i);
         
-        for(int i = 0; i < report.length; i++) {
-            String[] tmp = report[i].split(" ");
-            if(reports[user.get(tmp[0])][user.get(tmp[1])] == 1) continue;
-            reports[user.get(tmp[0])][user.get(tmp[1])] = 1;
-            counts[user.get(tmp[1])]++;
+        for(String s : report) {
+            String[] tmp = s.split(" ");
+            int tmp0 = user.get(tmp[0]); 
+            int tmp1 = user.get(tmp[1]);
+            if(reports[tmp0][tmp1] == 1) continue;
+            reports[tmp0][tmp1] = 1;
+            counts[tmp1]++;
         }
         
         for(int i = 0; i < id_list.length; i++) {
             if (counts[i] >= k) {
                 for(int j = 0; j < id_list.length; j++) {
-                    if(reports[j][i] > 0) {
-                        answer[j]++;
-                    }
+                    if(reports[j][i] > 0) answer[j]++;
                 }
             } 
         }     
