b43fdf80bbebeb01e467c6f6cc92c78a256cb20f
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-03-17 21:02:40 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.50 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = {51,51,0,0};
+        for(int i = 0; i < wallpaper.length; i++) {
+            for(int j = 0; j < wallpaper[i].length(); j++) {
+                if(wallpaper[i].substring(j,j+1).equals("#")) {
+                    answer[0] = Math.min(answer[0], i);
+                    answer[1] = Math.min(answer[1], j);
+                    answer[2] = Math.max(answer[2], i+1);
+                    answer[3] = Math.max(answer[3], j+1);
+                }
+            }
+        }
+        return answer;
+    }
+}
