c46e74bd0a3d9ff5ce5528a8518dd219fa41fb1a
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-04-18 16:41:41 +0900

    [level 2] Title: 타겟 넘버, Time: 4.24 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,82 @@
-import java.util.Stack;
+import java.util.*;
 
 public class Solution {
     public int solution(int[] numbers, int target) {
-        Stack<int[]> s = new Stack<>();
-        int count = 0;
-        s.push(new int[]{0,0});
-        while(!s.isEmpty()) {
-            int[] now = s.pop();
-            if(now[0] == numbers.length) {
-                if(now[1] == target) count++;
-                continue;
+        int answer = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0}); 
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int currentSum = current[0];
+            int currentIndex = current[1];
+
+            if (currentIndex == numbers.length) { 
+                if (currentSum == target) {
+                    answer++; 
+                }
+            } else {
+                int nextNumber = numbers[currentIndex];
+                queue.add(new int[]{currentSum + nextNumber, currentIndex + 1}); 
+                queue.add(new int[]{currentSum - nextNumber, currentIndex + 1}); 
             }
-            s.push(new int[]{now[0] + 1, now[1] + numbers[now[0]]});
-            s.push(new int[]{now[0] + 1, now[1] - numbers[now[0]]});
         }
-        return count;
+
+        return answer;
     }
 }
+
+
+//         Stack<State> s = new Stack<>();
+//         s.push(new State(0, 0));
+//         int count = 0;
+//         while (!s.isEmpty()) {
+//             State state = s.pop();
+//             if (state.index == numbers.length) {
+//                 if (state.acc == target) count++;
+//                 continue;
+//             }
+//             s.push(new State(state.index + 1, state.acc - numbers[state.index]));
+//             s.push(new State(state.index + 1, state.acc + numbers[state.index]));
+//         }
+//         return count;
+//     }
+    
+//     private static class State {
+//         public final int index;
+//         public final int acc;
+
+//         State(int index, int acc) {
+//             this.index = index;
+//             this.acc = acc;
+//         }
+//     }
+// }
+
+
+// class Solution {
+//     static int answer;
+//     static int[] numbers;
+//     static int target;
+    
+//     public int solution(int[] numbers, int target) {
+//         answer = 0;
+//         this.numbers = numbers;
+//         this.target = target;
+        
+//         dfs(0,0);
+//         return answer;
+//     }
+    
+//     public void dfs(int i, int sum){
+//         if(i == numbers.length){
+//             if(sum == target){
+//                 answer++;
+//             }
+//         return;
+//         }
+//         // 양갈래
+//         dfs(i+1,sum+numbers[i]);
+//         dfs(i+1,sum-numbers[i]);
+//     }
+// }
