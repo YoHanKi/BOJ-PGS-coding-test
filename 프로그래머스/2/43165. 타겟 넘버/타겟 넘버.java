014368e157246d18b81def8ef1211d65550bf03b
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-04-18 16:59:27 +0900

    [level 2] Title: 타겟 넘버, Time: 0.35 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,29 @@
 import java.util.*;
 
-public class Solution {
-    public int solution(int[] numbers, int target) {
-        int answer = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0}); 
+// public class Solution {
+//     public int solution(int[] numbers, int target) {
+//         int answer = 0;
+//         Queue<int[]> queue = new LinkedList<>();
+//         queue.add(new int[]{0, 0}); 
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int currentSum = current[0];
-            int currentIndex = current[1];
+//         while (!queue.isEmpty()) {
+//             int[] now = queue.poll();
+//             int sum = now[0];
+//             int index = now[1];
 
-            if (currentIndex == numbers.length) { 
-                if (currentSum == target) {
-                    answer++; 
-                }
-            } else {
-                int nextNumber = numbers[currentIndex];
-                queue.add(new int[]{currentSum + nextNumber, currentIndex + 1}); 
-                queue.add(new int[]{currentSum - nextNumber, currentIndex + 1}); 
-            }
-        }
-
-        return answer;
-    }
-}
+//             if (index == numbers.length) { 
+//                 if (sum == target) {
+//                     answer++; 
+//                 }
+//             } else {
+//                 int next = numbers[index];
+//                 queue.add(new int[]{sum + next, index + 1}); 
+//                 queue.add(new int[]{sum - next, index + 1}); 
+//             }
+//         }
+//         return answer;
+//     }
+// }
 
 
 //         Stack<State> s = new Stack<>();
@@ -54,29 +53,29 @@ public class Solution {
 // }
 
 
-// class Solution {
-//     static int answer;
-//     static int[] numbers;
-//     static int target;
+class Solution {
+    static int answer;
+    static int[] numbers;
+    static int target;
     
-//     public int solution(int[] numbers, int target) {
-//         answer = 0;
-//         this.numbers = numbers;
-//         this.target = target;
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
         
-//         dfs(0,0);
-//         return answer;
-//     }
+        dfs(0,0);
+        return answer;
+    }
     
-//     public void dfs(int i, int sum){
-//         if(i == numbers.length){
-//             if(sum == target){
-//                 answer++;
-//             }
-//         return;
-//         }
-//         // 양갈래
-//         dfs(i+1,sum+numbers[i]);
-//         dfs(i+1,sum-numbers[i]);
-//     }
-// }
+    public void dfs(int i, int sum){
+        if(i == numbers.length){
+            if(sum == target){
+                answer++;
+            }
+        return;
+        }
+        // 양갈래
+        dfs(i+1,sum+numbers[i]);
+        dfs(i+1,sum-numbers[i]);
+    }
+}
