69ec42fe31d7e8132caf81c686e525d08be0dd91
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-04-22 17:05:59 +0900

    [level 3] Title: 네트워크, Time: 3.51 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,16 +1,54 @@
 import java.util.*;
+import java.util.stream.*;
+
+// class Solution {
+//     static HashSet<Integer>[] net;
+//     static int[] visited;
+//     static int count = 1;
+//     public int solution(int n, int[][] computers) {
+//         net = new HashSet[n+1];
+//         visited = new int[n+1];
+        
+//         for(int i = 0; i <= n; i++) {
+//             net[i] = new HashSet<>();
+//         }
+        
+//         for (int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(computers[i][j] == 1) {
+//                     net[i+1].add(j+1);
+//                     net[j+1].add(i+1);
+//                 }
+//             }
+//         }
+        
+//         for(int i = 1; i <= n; i++) {
+//             dfs(i);
+//             if(visited[i] == count) count++;
+//         }
+        
+//         return Arrays.stream(visited).max().getAsInt();
+//     }
+    
+//     void dfs(int i) {
+//         //종료조건
+//         if(visited[i] != 0) return;
+//         visited[i] = count;
+        
+//         for(int a : net[i]) {
+//         dfs(a);
+//         }
+//     }
+// }
 
 class Solution {
     static HashSet<Integer>[] net;
     static int[] visited;
-    static int count = 1;
     public int solution(int n, int[][] computers) {
         net = new HashSet[n+1];
         visited = new int[n+1];
         
-        for(int i = 0; i <= n; i++) {
-            net[i] = new HashSet<>();
-        }
+        for(int i = 0; i <= n; i++) net[i] = new HashSet<>();
         
         for (int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
@@ -21,27 +59,24 @@ class Solution {
             }
         }
         
+        int check = 1;
         for(int i = 1; i <= n; i++) {
-            dfs(i);
-            count++;
+            bfs(i, check);
+            if(visited[i] == check) check++;
         }
         
-        Set<Integer> set = new HashSet<>();
-        for(int i = 1; i <= n; i++) {
-            System.out.println(visited[i]);
-            set.add(visited[i]);
-        }
-        
-        return set.size();
+        return Arrays.stream(visited).max().getAsInt();
     }
     
-    void dfs(int i) {
-        //종료조건
-        if(visited[i] != 0) return;
-        visited[i] = count;
-        
-        for(int a : net[i]) {
-        dfs(a);
+    void bfs(int i, int check) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int j : net[i]) q.offer(j);
+        while(!q.isEmpty()) {
+            int j = q.poll();
+            if(visited[j] == 0) {
+                visited[j] = check;
+                for(int k : net[j]) q.offer(k);
+            }
         }
     }
 }
