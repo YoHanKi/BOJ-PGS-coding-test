a6e3e649f49d83a6a44fc89f8e337b8b869b8465
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-07-04 13:19:34 +0900

    [level 2] Title: 땅따먹기, Time: 14.66 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,24 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        int[][] dp = new int[land.length][land[0].length];
+        for (int i = 0; i < 4; i++) {
+            dp[land.length-1][i] = land[land.length-1][i];
+        }
+        
+        //바텀 업 방식으로 아래행부터 시작
+        for(int i = land.length - 2; i >= 0; i--) {
+            for(int j = 0; j < 4; j++) {
+                for(int k = 0; k < 4; k++) {
+                    //같은 열은 제외
+                    if(j==k) continue;
+                    dp[i][j] = Math.max(dp[i][j], dp[i + 1][k] + land[i][j]);
+                }
+            }
+        }
+        
+        for(int i = 0; i < 4; i++) answer = Math.max(answer, dp[0][i]);
+        
+        return answer;
+    }
+}
