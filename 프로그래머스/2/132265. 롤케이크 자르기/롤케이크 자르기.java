f022be97b8fada7d37a4e46d9e0ad3cedba414c1
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-08-17 00:48:39 +0900

    [level 2] Title: 롤케이크 자르기, Time: 105.06 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,25 +1,36 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int solution(int[] topping) {
-        int answer = 0;
+        int n = topping.length;
+
+        // Prefix와 Suffix 계산을 위한 Set과 배열 선언
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
         Set<Integer> set = new HashSet<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int i : topping) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+
+        // Prefix 배열 계산
+        for (int i = 0; i < n; i++) {
+            set.add(topping[i]);
+            prefix[i] = set.size();
         }
-        
-        for(int i = 0; i < topping.length; i++) {
-            int number = topping[i];
-            set.add(number);
-            map.put(number, map.get(number) - 1);
-            
-            if(map.get(number) == 0) map.remove(number);
-            
-            if(set.size() == map.size()) answer++;
+
+        set.clear();
+
+        // Suffix 배열 계산
+        for (int i = n - 1; i >= 0; i--) {
+            set.add(topping[i]);
+            suffix[i] = set.size();
         }
-        
-        return answer;
+
+        // Prefix와 Suffix 비교
+        int result = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (prefix[i] == suffix[i + 1]) {
+                result++;
+            }
+        }
+
+        return result;
     }
 }
