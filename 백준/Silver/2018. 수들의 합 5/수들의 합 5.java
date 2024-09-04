6226ab359dd4624a2ed33259b9e5bc36062b2609
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-09-04 14:39:48 +0900

    [Silver V] Title: 수들의 합 5, Time: 144 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -7,27 +7,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        //투포인터 활용
-        int stIndex = 1;
-        int edIndex = 1;
-        //N, 스스로는 합이 N이기 때문에 N을 제외하기 위해 count 1 부터 시작
+
         int count = 1;
         int sum = 1;
-        //enIndex가 N이 된다면 종료
-        while(edIndex != N) {
-            //합이 N일 시 count++, edIndex 이동
+        int left = 1;
+        int right = 1;
+        while(right != N) {
             if(sum == N) {
                 count++;
-                edIndex++;
-                sum += edIndex;
-            }
-            //합이 N보다 클 시 stIndex 이동
-            else if (sum > N) {
-                sum -= stIndex;
-                stIndex++;
+                sum += ++right;
+            } else if(sum < N) {
+                sum += ++right;
             } else {
-                edIndex++;
-                sum += edIndex;
+                sum -= left++;
             }
         }
 
