25197ce7291e20f43c428655b5b73d18ae5e826c
기묘한 주니어 개발자 <139758405+YoHanKi@users.noreply.github.com>
2024-09-05 13:13:40 +0900

    [Silver III] Title: 칸토어 집합, Time: 164 ms, Memory: 18020 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -2,6 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 반복된 숫자가 나올 가능성과, 이전 데이터를 사용하는 것이 효과적이라고 생각하여 DP 선택
+ * -------------------------------------------------------------------------
+ *
+ */
 public class Main {
     static String[] dp = new String[13];
 
@@ -12,13 +17,17 @@ public class Main {
         while ((str = br.readLine()) != null) {
             int N = Integer.parseInt(str);
             if(dp[N] == null) {
-                for(int i = 1; i < dp.length; i++) {
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(dp[i-1]).append(" ".repeat(dp[i-1].length())).append(dp[i-1]);
-                    dp[i] = sb.toString();
-                }
+                cantoring(N);
             }
             System.out.println(dp[N]);
+        }
+    }
+    public static void cantoring(int N) {
+        for(int i = 1; i <= N; i++) {
+            if(dp[i] != null) continue;
+            StringBuilder sb = new StringBuilder();
+            sb.append(dp[i-1]).append(" ".repeat(dp[i-1].length())).append(dp[i-1]);
+            dp[i] = sb.toString();
         }
     }
 }
